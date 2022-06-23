45ee65923b348dbbec6e00e2981df2aa61471536
Olga Smirnova <olya_fcsm@mail.ru>
2022-06-23 19:46:14 +0300

    first

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+export default function getSpecialAttack({ special }) {
+  const result = [];
+
+  for (const elem of special) {
+    const {
+      description = 'Описание недоступно', ...rest
+    } = elem;
+    result.push({
+      ...rest, description,
+    });
+  }
+
+  return result;
+}
